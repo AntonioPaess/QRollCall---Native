a2d6e8e7d798a829d0592ca206153f5b66a9bcda
AntonioPaess <ajpaj@cesar.org.br>
2026-04-08 11:17:42 -0300

    Initial Commit

---
+++ QRollCallTests/QRollCallTests.swift
@@ -0,0 +1,17 @@
+//
+//  QRollCallTests.swift
+//  QRollCallTests
+//
+//  Created by Antônio Paes on 08/04/26.
+//
+
+import Testing
+@testable import QRollCall
+
+struct QRollCallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
